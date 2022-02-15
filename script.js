2d35fce2bb1cc7ebc2f97e28a019902bba93b819
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2022-02-15 09:09:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cloud Professional", "Programmer", "Tech Enthusiast"],
+        strings: ["Cloud Developer", "Python Programmer", "Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud Professional", "Programmer", "Tech Enthusiast"],
+        strings: ["Cloud Developer", "Python Programmer", "Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
